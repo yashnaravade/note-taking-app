de95968b0381790d4f7523bc8139c2e4bd2d604a
Yash Naravade <yashdip123@gmail.com>
2022-12-14 00:37:00 +0530

    Add local storage hook

--- src/useLocalStorage.ts
+++ src/useLocalStorage.ts
@@ -1,28 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react"
 
-export function useLocalStorage<T>(key: string, initialValue: T) {
-  const [storedValue, setStoredValue] = useState<T>(() => {
-    try {
-      const item = window.localStorage.getItem(key);
-      return item ? JSON.parse(item) : initialValue;
-    } catch (error) {
-      console.log(error);
-      return initialValue;
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+    const jsonValue = localStorage.getItem(key)
+    if (jsonValue == null) {
+      if (typeof initialValue === "function") {
+        return (initialValue as () => T)()
+      } else {
+        return initialValue
+      }
+    } else {
+      return JSON.parse(jsonValue)
     }
-  });
+  })
 
-  const setValue = (value: T | ((val: T) => T)) => {
-    try {
-      const valueToStore =
-        value instanceof Function ? value(storedValue) : value;
-      setStoredValue(valueToStore);
-      window.localStorage.setItem(key, JSON.stringify(valueToStore));
-    } catch (error) {
-        console.log(error);
-        }
-    };
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [value, key])
 
-    return [storedValue, setValue];
+  return [value, setValue] as [T, typeof setValue]
 }
-
-
