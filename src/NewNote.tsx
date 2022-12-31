85b747aff1d3661be6d7efcdeee7fddec2c064c9
Yash Naravade <yashdip123@gmail.com>
2023-01-01 02:16:53 +0530

    Add dark mode

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,5 +1,9 @@
+import { Button, Col, Row, Stack } from "react-bootstrap"
 import { NoteData, Tag } from "./App"
 import { NoteForm } from "./NoteForm"
+import { Link } from "react-router-dom"
+import "./App.css"
+import { useEffect, useState } from "react"
 
 type NewNoteProps = {
   onSubmit: (data: NoteData) => void
@@ -9,8 +13,13 @@ type NewNoteProps = {
 
 export function NewNote({ onSubmit, onAddTag, availableTags }: NewNoteProps) {
   return (
+
     <>
-      <h1 className="mb-4">New Note</h1>
+       <Row className="align-items-center mb-4">
+        <Col>
+          <h1>New Note</h1>
+        </Col>
+      </Row>
       <NoteForm
         onSubmit={onSubmit}
         onAddTag={onAddTag}
